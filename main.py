11c695bbb7d863ed7770a1e035f47a6c440f1efe
angelinaaat <147929001+angelinaaat@users.noreply.github.com>
2023-10-21 15:14:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from password import Ui_MainWindow
+import random
 
 class Widget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.btn_OK.clicked.connect(self.generate)
+
+    def generate(self):
+        s_number = '0123456789'
+        s_alphabet = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+        pas = ''
+        if self.ui.cb_number.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_number)
+        
+        if self.ui.cb_alphabet.isChecked():
+            for i in range(16):
+                pas = pas + random.choice(s_alphabet)
+
+
+        if self.ui.cb_number.isChecked() and self.ui.cb_alphabet.isClicked():
+            for i in range(16):
+                pas = pas + random.choice(s_number + s_alphabet)
+
+        self.ui.result.setText(pas)
+
 
 pushButton.clicked.connect(generation)
 
 app = QApplication([])
 ex = Widget()
-
-def generation():
-    digital = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    letter = ['g', 'l', 'm', 'q', 'w', 't', 'r', 'g', 'y', 'p']
-
-
 
 pushButton.clicked.connect(generation)
  
